59ebbeda8802d4959375bd960676f3216bcb073d
ewerttrewe <szymonewert@gmail.com>
2023-04-29 03:14:38 +0200

    chore: setup nginx react with docker

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Real Estate site</h1>
+      <p>test for frontend connection</p>
+    </div>
+  );
+}
+
+export default App;
